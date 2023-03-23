9b32fcbd4326a4b6a497aa164042ae9db29f1b1f
cymonz54 <cymonzchanua54@gmail.com>
2023-03-23 13:44:58 -0700

    correction

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * more_numbers - Print 10 times the numbers since 0 up to 14
- * Return: 10 times of the numbers since 0 up to 14
+ * more_numbers - Print 10 times the numbers from 0 up to 14
+ * Return: 10 times of the numbers from 0 up to 14
  */
 
 void more_numbers(void)
@@ -14,7 +14,7 @@ void more_numbers(void)
 	{
 		for (y = 0; y <= 14; y++)
 		{
-			if (y > 0)
+			if (y > 9)
 			{
 				_putchar((y / 10) + '0');
 			}
